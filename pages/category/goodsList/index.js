edb8783e0ab2680a7ecfb54ef544c2f8c24aa762
dengtao6200594 <dengtao6200594@126.com>
2021-08-18 23:12:00 +0800

    818

--- pages/category/goodsList/index.js
+++ pages/category/goodsList/index.js
@@ -6,8 +6,16 @@ Page({
      * 页面的初始数据
      */
     data: {
-        tabs: [{ id: 0, value: '综合', isActive: true }, { id: 1, value: '销量', isActive: false }, { id: 2, value: '价格', isActive: false }]
+        tabs: [{ id: 0, value: '综合', isActive: true }, { id: 1, value: '销量', isActive: false }, { id: 2, value: '价格', isActive: false }],
+        goodLists: []
     },
+    params: {
+        cid: null,
+        pagenum: 1,
+        pagesize: 10,
+    },
+    total: null,
+
     handleTabsChange(e) {
         const { index } = e.detail
         const _tabs = this.data.tabs.map((v, i) => {
@@ -17,12 +25,23 @@ Page({
         this.setData({ tabs: _tabs })
     },
     /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+        this.params.pagenum < Math.ceil(this.total / this.params.pagesize) &&
+            this.params.pagenum++ &&
+            getGoodsSearch(this.params).then(({ message }) => this.setData({ goodLists: [...this.data.goodLists, ...message.goods] })) ||
+            wx.showToast({ title: '我的忍耐是有极限的' })
+    },
+    /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        const { cid } = options
-        cid && getGoodsSearch({ cid, pagenum: 1, pagesize: 5 }).then(({ message }) => {
-            console.log(message)
+        this.params.cid = options.cid
+        this.params.cid && getGoodsSearch(this.params).then(({ message }) => {
+            const { goods, total } = message
+            this.setData({ goodLists: goods })
+            this.total = total
         })
 
     },
@@ -62,12 +81,7 @@ Page({
 
     },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
 
-    },
 
     /**
      * 用户点击右上角分享
